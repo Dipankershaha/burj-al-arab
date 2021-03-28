24990f0f01f4db73e91afb009c6359f26b7c8098
Dipankershaha <dipankershaha@gmail.com>
2021-03-28 22:01:13 +0600

    This is a Hotel room booking website frontend

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_kmpYz0U7MWYHnP0SJuSNHN-X7uuEWB0",
+    authDomain: "burj-al-arab-678e2.firebaseapp.com",
+    projectId: "burj-al-arab-678e2",
+    storageBucket: "burj-al-arab-678e2.appspot.com",
+    messagingSenderId: "1023745100344",
+    appId: "1:1023745100344:web:d81f1becba5ca3406e74cd"
+  };
+  // Initialize Firebase
+  export default firebaseConfig;
